9b941fd83ba863951e4a425376a8c5167e6c8c92
Moshiqu <912323520@qq.com>
2024-04-16 23:12:07 +0800

    七彩虹数据库密码

--- sql/db.config.js
+++ sql/db.config.js
@@ -1,7 +1,8 @@
 module.exports = {
-  host     : 'localhost',
-  user     : 'root',
-  password : '123456',
-  database : 'menu',
-  connectionLimit: 10, // 连接池最大连接数
+    host: 'localhost',
+    user: 'root',
+    //   password : '123456',
+    password: 'admin',
+    database: 'menu',
+    connectionLimit: 10, // 连接池最大连接数
 }
