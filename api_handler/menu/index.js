9e1e6594387fc8afcb4d05523b399a14bc594a1d
Moshiqu <912323520@qq.com>
2024-04-25 23:08:21 +0800

    修改获取菜单接口

--- api_handler/menu/index.js
+++ api_handler/menu/index.js
@@ -13,8 +13,10 @@ exports.getMenuHandler = (req, res) => {
             category_id = category_id[0]
         }
 
+        const placeholders = Array(category_id.length).fill('?').join(','); // 生成占位符字符串，例如 '?,?,?,?,?' 
+
         db.query(
-            `SELECT category_id, id, img_src, product_description, product_name, sold_num, sort_index, product_price FROM production WHERE category_id in ${typeof category_id === 'object' ? '(?,?)' : '(?)'} AND is_active = 1 ORDER BY sort_index ASC`,
+            `SELECT category_id, id, img_src, product_description, product_name, sold_num, sort_index, product_price FROM production WHERE category_id in (${placeholders}) AND is_active = 1 ORDER BY sort_index ASC`,
             category_id,
             (err, productions) => {
                 if (err) return res.output(500, err.code)
