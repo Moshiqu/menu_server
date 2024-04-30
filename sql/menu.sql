77722c83141b1bbe5636e8754527270003f8c0f8
liyuan <912323520@qq.com>
2024-04-30 17:38:53 +0800

    修改sql

--- sql/menu.sql
+++ sql/menu.sql
@@ -1,17 +1,17 @@
 /*
  Navicat Premium Data Transfer
 
- Source Server         : Mysql
+ Source Server         : menu
  Source Server Type    : MySQL
- Source Server Version : 80033
+ Source Server Version : 80036 (8.0.36)
  Source Host           : localhost:3306
  Source Schema         : menu
 
  Target Server Type    : MySQL
- Target Server Version : 80033
+ Target Server Version : 80036 (8.0.36)
  File Encoding         : 65001
 
- Date: 27/04/2024 17:24:57
+ Date: 30/04/2024 17:36:42
 */
 
 SET NAMES utf8mb4;
@@ -59,7 +59,7 @@ CREATE TABLE `material`  (
   `material_name` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '材料名称',
   `material_quantity` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '材料用量',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 68 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = DYNAMIC;
+) ENGINE = InnoDB AUTO_INCREMENT = 74 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
 -- Records of material
@@ -74,6 +74,49 @@ INSERT INTO `material` VALUES (70, 0, 2, '开水', '2升');
 INSERT INTO `material` VALUES (71, 1, 2, '莴笋', '100g');
 INSERT INTO `material` VALUES (72, 1, 2, '削皮刀', '');
 INSERT INTO `material` VALUES (73, 1, 2, '开水', '2升');
+
+-- ----------------------------
+-- Table structure for order_products
+-- ----------------------------
+DROP TABLE IF EXISTS `order_products`;
+CREATE TABLE `order_products`  (
+  `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '订单内商品id',
+  `is_active` int UNSIGNED NOT NULL DEFAULT 1 COMMENT '是否删除，1：未删除；0：已删除',
+  `created_time` datetime NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `update_time` datetime NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+  `product_id` bigint NOT NULL COMMENT '商品id',
+  `product_num` int NOT NULL COMMENT '商品数量',
+  `order_id` char(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '所属订单id',
+  `product_price` decimal(10, 2) NULL DEFAULT NULL COMMENT '订单生成时, 商品价格',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 9 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of order_products
+-- ----------------------------
+INSERT INTO `order_products` VALUES (7, 1, '2024-04-30 17:32:16', '2024-04-30 17:32:16', 81, 123, '4c5b576057c64bc791c87263ec5a1d78', 99.00);
+INSERT INTO `order_products` VALUES (8, 1, '2024-04-30 17:32:16', '2024-04-30 17:32:16', 84, 456, '4c5b576057c64bc791c87263ec5a1d78', 199.20);
+
+-- ----------------------------
+-- Table structure for orders
+-- ----------------------------
+DROP TABLE IF EXISTS `orders`;
+CREATE TABLE `orders`  (
+  `id` varchar(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '订单id',
+  `is_active` int UNSIGNED NOT NULL DEFAULT 1 COMMENT '是否删除，1：未删除；0：已删除',
+  `created_time` datetime NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `update_time` datetime NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+  `order_status` int NOT NULL DEFAULT 1 COMMENT '订单状态，1：待制作；2：制作中；3：制作完成；4：订单异常；5：待评价；6：订单完成',
+  `order_price` decimal(10, 2) NOT NULL DEFAULT 0.00 COMMENT '订单价格',
+  `owner_id` bigint NOT NULL COMMENT '商家id',
+  `consumer_id` bigint NOT NULL COMMENT '消费者id',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of orders
+-- ----------------------------
+INSERT INTO `orders` VALUES ('4c5b576057c64bc791c87263ec5a1d78', 1, '2024-04-30 17:32:16', '2024-04-30 17:32:16', 1, 0.00, 10086, 10011);
 
 -- ----------------------------
 -- Table structure for production
@@ -126,7 +169,7 @@ CREATE TABLE `step`  (
   `step_img` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '步骤图片',
   `step_index` int NOT NULL DEFAULT 1 COMMENT '步骤数',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 48 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = DYNAMIC;
+) ENGINE = InnoDB AUTO_INCREMENT = 51 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
 -- Records of step
@@ -138,5 +181,24 @@ INSERT INTO `step` VALUES (47, 1, 91, '2', '', 1);
 INSERT INTO `step` VALUES (48, 0, 2, '切莴笋', '', 1);
 INSERT INTO `step` VALUES (49, 1, 2, '切莴笋', '', 1);
 INSERT INTO `step` VALUES (50, 1, 2, '吃莴笋', '', 2);
+
+-- ----------------------------
+-- Table structure for user
+-- ----------------------------
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user`  (
+  `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '用户id',
+  `is_active` int UNSIGNED NOT NULL DEFAULT 1 COMMENT '是否删除，1：未删除；0：已删除',
+  `created_time` datetime NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `update_time` datetime NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+  `nick_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '用户昵称',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 10087 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of user
+-- ----------------------------
+INSERT INTO `user` VALUES (10011, 1, '2024-04-30 11:50:57', '2024-04-30 11:50:57', '大青蛙');
+INSERT INTO `user` VALUES (10086, 1, '2024-04-30 11:50:40', '2024-04-30 11:50:40', '小松鼠');
 
 SET FOREIGN_KEY_CHECKS = 1;
