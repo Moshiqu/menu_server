870dbbe770ba5c5d6ee0765ba192cbf9048a0f1d
liyuan <912323520@qq.com>
2024-05-24 10:18:50 +0800

    1

--- sql/db.config.js
+++ sql/db.config.js
@@ -1,8 +1,8 @@
 module.exports = {
     host: 'localhost',
     user: 'root',
-    //   password : '123456',
-    password: 'admin',
+      password : '123456',
+    // password: 'admin',
     database: 'menu',
     connectionLimit: 10, // 连接池最大连接数
 }
